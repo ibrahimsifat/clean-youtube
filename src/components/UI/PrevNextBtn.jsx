7c40d4b6787ce88d8cd900e4c455d7367f76991f
ibrahimsifat <ibsifat24@gmail.com>
2023-02-06 12:16:36 +0300

    added-video-next-prev-player

---
+++ src/components/UI/PrevNextBtn.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const PrevNextBtn = ({ prev, level }) => {
+  return (
+    <button
+      type="button"
+      class="bg-gray-800 text-white rounded-r-md py-2 border-l border-gray-200 hover:bg-red-700 hover:text-white px-3"
+    >
+      <div class="flex flex-row align-middle">
+        <span class="mr-2">{level}</span>
+        {prev ? (
+          <svg
+            class="w-5 mr-2"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        ) : (
+          <svg
+            class="w-5 ml-2"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        )}
+      </div>
+    </button>
+  );
+};
+
+export default PrevNextBtn;
--- src/components/videos/VideoPlayer.jsx
+++ src/components/videos/VideoPlayer.jsx
@@ -1,31 +1,94 @@
-import React from "react";
+import { useStoreState } from "easy-peasy";
+import React, { useRef, useState } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import YouTube from "react-youtube";
+import { calculateTime } from "../../utils/time";
+import PrevNextBtn from "../UI/PrevNextBtn";
 
 const VideoPlayer = () => {
   const { videoId } = useParams();
+  const { currentPlaylist } = useStoreState((state) => state.playlist);
+  const [event, setEvent] = useState();
 
+  const playerRef = useRef();
+  const [startTime, setStartTime] = useState(0);
   const onPlayerReady = (event) => {
     // access to player in all event handlers via event.target
     event.target.playVideo();
   };
+  const onChange = (e) => {
+    if (
+      e.data === YouTube?.PlayerState.PLAYING ||
+      e.data === YouTube?.PlayerState.UNSTARTED
+    ) {
+      setEvent(e);
+    }
+
+    if (
+      e.data === YouTube?.PlayerState.PAUSED ||
+      e.data === YouTube?.PlayerState.ENDED
+    ) {
+      setEvent(e);
+    }
+
+    return;
+  };
+  console.log(calculateTime(event?.target.getDuration()));
+  console.log(calculateTime(event?.target.getCurrentTime()));
+  console.log(event?.target.nextVideo());
 
   const opts = {
     height: "100%",
     width: "100%",
     playerVars: {
       autoplay: 1,
+      rel: 0,
+      start: startTime,
     },
   };
+  // next and previous video handlers
+  const { playlistItems } = currentPlaylist || {};
+  // playlistItems?.forEach((video) => {
+  //   if (video.contentDetails.videoId === videoId) {
+
+  //     console.log(video);
+  //   }
+  // });
+  const currentPlaylistIndex = playlistItems?.findIndex(
+    (video) => video?.contentDetails.videoId === videoId
+  );
+  const prevPlaylistIndex = currentPlaylistIndex - 1;
+  const prevVideoId = playlistItems[prevPlaylistIndex]?.contentDetails.videoId;
+  const nextPlaylistIndex = currentPlaylistIndex + 1;
+  const nextVideoId = playlistItems[nextPlaylistIndex]?.contentDetails.videoId;
 
   return (
-    <YouTube
-      data-placeholder
-      videoId={videoId}
-      opts={opts}
-      className=" mx-auto 2xl:w-10/12 xl:w-10/12 2xl:h-[723px] xl:h-[604px] lg:h-[583px] md:h-[435px] sm:h-[363px] videoQuire w-full p-1 rounded bg-gradient-to-r from-teal-50 to-rose-50 bg-opacity-20 relative"
-      onReady={onPlayerReady}
-    />
+    <>
+      <YouTube
+        data-placeholder
+        videoId={videoId}
+        opts={opts}
+        className=" mx-auto 2xl:w-10/12 xl:w-10/12 2xl:h-[723px] xl:h-[604px] lg:h-[583px] md:h-[435px] sm:h-[363px] videoQuire w-full p-1 rounded bg-gradient-to-r from-teal-50 to-rose-50 bg-opacity-20 relative"
+        onReady={onPlayerReady}
+        onStateChange={onChange}
+        // getCurrentTime={(im) => console.log(im)}
+        // onEnd={onEnd}
+        ref={playerRef}
+      />
+      <div className="flex justify-between items-center my-3">
+        {prevVideoId && (
+          <Link to={`/video/${prevVideoId}`}>
+            <PrevNextBtn prev level="Prev" />
+          </Link>
+        )}
+        {nextVideoId && (
+          <Link to={`/video/${nextVideoId}`}>
+            <PrevNextBtn level="Next" />
+          </Link>
+        )}
+      </div>
+    </>
   );
 };
 
